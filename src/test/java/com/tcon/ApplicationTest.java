279bbf2f44d14031e6282ac30ed939b9d112524c
fatihtokus <fatih.tokus@gmail.com>
2023-01-25 23:30:27 +0000

    wip

--- src/test/java/com/tcon/ApplicationTest.java
+++ src/test/java/com/tcon/ApplicationTest.java
@@ -1,22 +1,4 @@
 package com.tcon;
 
-import com.google.gson.Gson;
-import com.google.gson.JsonArray;
-import com.google.gson.JsonObject;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
-
-import java.io.*;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.time.Instant;
-import java.util.Arrays;
-import java.util.List;
-import java.util.stream.Collectors;
-
-import static java.util.Arrays.asList;
-import static java.util.stream.Collectors.joining;
-
 public class ApplicationTes {
 }
